0ecb13950248156bb0b42ad32af25383f16108b1
manhkun <kunpro1st@gmail.com>
2020-09-17 20:51:48 +0700

    fix

--- components/HomeQuiz.js
+++ components/HomeQuiz.js
@@ -3,7 +3,6 @@ import {View, Text, Dimensions, StyleSheet, SafeAreaView, Image, Keyboard, Touch
     FlatList, ScrollView, TextInput, TouchableOpacity} from 'react-native';
 import Quiz from './Quiz';
 import TaskBar from './TaskBar';
-<<<<<<< HEAD
 import Header from './Header';
 
 const screen = Dimensions.get('window');
@@ -17,30 +16,12 @@ export default class HomeQuiz extends Component {
     }
     render(){
         const {nameTab} = this.props;
-=======
-const screen = Dimensions.get('window');
-
-export default class HomeQuiz extends Component {
-    render(){
->>>>>>> 840068a92d701e0e0a316029ff24b3e46708721c
         return(  
         <SafeAreaView style={styles.container}>
             <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
                 <ScrollView showsVerticalScrollIndicator={false}>
                     <View style={styles.container}>
-<<<<<<< HEAD
                         <Header nameTab={this.state.nameTab}/>
-=======
-                        <View style={styles.header}>
-                            <Text style={styles.homeText}>Home</Text>
-                            <TouchableOpacity style={styles.menu}>
-                                <View style={styles.menuButton}>
-                                    <Image source={require('../images/menu-button.png')}
-                                            style={{width: 20, height: 20}}></Image>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
->>>>>>> 840068a92d701e0e0a316029ff24b3e46708721c
                         <View style={styles.joinGame}>
                             <TextInput 
                                 style={styles.inputGameCode}
@@ -112,28 +93,6 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
     },
-<<<<<<< HEAD
-=======
-    header: {
-        width: screen.width,
-        height: 66,
-        flexDirection: 'row',
-        position: "relative"
-    },
-    homeText: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        position: 'absolute',
-        marginLeft: 12,
-        top: 10
-    },
-    menu:{
-        marginRight: 12,
-        position: 'absolute',
-        right:10,
-        top: 10
-    },
->>>>>>> 840068a92d701e0e0a316029ff24b3e46708721c
     menuButton: {
         width: 40,
         height: 40,
